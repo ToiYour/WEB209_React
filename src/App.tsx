d47afd683d1e7c50f07b5320f7621e9959ee20e6
ToiYour <toidz25102004@gmail.com>
2024-06-27 20:52:16 +0700

    CRUD TODO LIST

--- src/App.tsx
+++ src/App.tsx
@@ -1,113 +1,176 @@
-import axios from "axios";
-import { useEffect, useRef, useState } from "react";
-type Todo = {
-  id?: number;
-  name?: string;
-};
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import clsx from "clsx";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+
 function App() {
-  const inputRef = useRef<HTMLInputElement | null>(null);
-  const [todo, setTodo] = useState("");
-  const [id, setId] = useState<number | undefined>(undefined);
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<any[]>([]);
+  const [isOpen, setIsOpen] = useState({
+    open: false,
+    id: null,
+  });
+
+  const RegisterForm = () => {
+    const {
+      register,
+      handleSubmit,
+      reset,
+      formState: { errors },
+    } = useForm<{ name: string; id?: number; action?: string }>();
+    return { register, handleSubmit, reset, errors };
+  };
+  const forms = {
+    add: RegisterForm(),
+    update: RegisterForm(),
+  };
   useEffect(() => {
-    (async () => {
-      const { data } = await axios.get(`http://localhost:3000/todos`);
-      setTodos(data);
-    })();
-  }, []);
-  const setTodoInput = (todo: string) => {
-    setTodo(todo);
-  };
-  const addTodo = async () => {
-    const { data } = await axios.post(`http://localhost:3000/todos`, {
-      name: todo,
-    });
-    setTodos((prev) => [...prev, data]);
-    setTodo("");
-    inputRef.current && inputRef.current.focus();
-  };
-  const deleteTodo = async (id: number) => {
-    await axios.delete(`http://localhost:3000/todos/${id}`);
-    setTodos((prev) => {
-      const result = prev.filter((todo) => todo.id != id);
-      return result;
-    });
-  };
-  const updateTodo = async () => {
-    await axios.put(`http://localhost:3000/todos/${id}`, { name: todo });
-    setTodos((prev) => {
-      const result = prev.map((t) => {
-        if (t.id == id) {
-          t.name = todo;
-        }
-        return t;
+    fetch(`http://localhost:3000/todos`)
+      .then((response) => response.json())
+      .then((data) => {
+        setTodos(data);
       });
-      return result;
-    });
-    setTodo("");
-    setId(undefined);
+  }, []);
+  const handleDelete = (id: number) => {
+    if (confirm("Bạn chắc chắn xoá chứ")) {
+      fetch(`http://localhost:3000/todos/${id}`, {
+        method: "DELETE",
+      })
+        .then((response) => response.json())
+        .then(() => {
+          setTodos(todos.filter((p) => p.id != id));
+        });
+    }
+  };
+  const handleAdd = (name: string) => {
+    fetch(`http://localhost:3000/todos`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setTodos((prev) => [...prev, data]);
+      });
+  };
+  const handleUpdate = (id: number, name: string) => {
+    fetch(`http://localhost:3000/todos/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name }),
+    })
+      .then((response) => response.json())
+      .then(() => {
+        setTodos((prev) =>
+          prev.map((todo) => {
+            if (todo.id == id) {
+              todo.name = name;
+            }
+            return todo;
+          })
+        );
+      });
+  };
+  const onSubmit = (payload: {
+    name: string;
+    id?: number;
+    action?: string;
+  }) => {
+    if (payload.action == "add") {
+      handleAdd(payload.name);
+      forms.add.reset();
+    } else {
+      handleUpdate(payload.id!, payload.name);
+      forms.update.reset();
+      setIsOpen({ open: false, id: null });
+    }
   };
   return (
-    <div className="flex flex-col items-start justify-center gap-5 m-5">
-      <div className="flex items-center gap-2 h-10">
-        <label
-          htmlFor="todo"
-          className="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+    <div className="mt-10 mx-auto w-1/2 space-y-10">
+      <form
+        className="flex items-center gap-2 "
+        onSubmit={forms.add.handleSubmit(onSubmit)}
+      >
+        <input
+          {...forms.add.register("name", {
+            required: "Vui lòng nhập công việc",
+          })}
+          type="text"
+          placeholder="Thêm công việc..."
+          className={clsx(
+            "border  p-2 rounded outline-none",
+            forms.add.errors?.name ? "border-red-500" : "border-gray-300"
+          )}
+        />
+        <input
+          type="text"
+          defaultValue={"add"}
+          {...forms.add.register("action")}
+          hidden
+        />
+        <button
+          type="submit"
+          className="text-white py-2 px-5 bg-blue-500 rounded"
         >
-          <input
-            ref={inputRef}
-            value={todo}
-            onChange={(e) => setTodoInput(e.target.value)}
-            type="text"
-            id="todo"
-            placeholder="Công việc"
-            className="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
-          />
-
-          <span className="absolute start-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
-            Công việc
-          </span>
-        </label>
-
-        {id ? (
-          <button
-            onClick={updateTodo}
-            className={"h-full px-5 bg-blue-500 rounded text-white"}
-          >
-            Cập nhập
-          </button>
-        ) : (
-          <button
-            onClick={addTodo}
-            className="h-full px-5 bg-blue-500 rounded text-white"
-          >
-            Thêm
-          </button>
-        )}
-      </div>
-
-      <ul className="">
-        {todos.map((item) => (
-          <li key={item.id} className="flex items-center gap-2 text-[#333]">
-            <input type="checkbox" className="peer" />
-            <span className="peer-checked:line-through capitalize">
-              {item.name}
-            </span>
+          Thêm mới
+        </button>
+      </form>
+      <ul className="space-y-5">
+        {todos.map((t: any) => (
+          <li key={t.id}>
             <span
-              onClick={() => deleteTodo(item.id as number)}
-              className=" hidden peer-checked:inline-block cursor-pointer "
+              onClick={() => setIsOpen({ open: true, id: t.id })}
+              className="cursor-pointer"
             >
+              {t.name}{" "}
+            </span>
+            <span onClick={() => handleDelete(t.id)} className="cursor-pointer">
               &times;
             </span>
-            <span
-              onClick={() => {
-                setId(item.id);
-                setTodo(item.name as string);
-              }}
-              className="hidden peer-checked:inline-block cursor-pointer"
-            >
-              Sửa
-            </span>
+            {isOpen.open && isOpen.id == t.id && (
+              <form
+                onSubmit={forms.update.handleSubmit(onSubmit)}
+                className="mt-1 flex items-center gap-2"
+              >
+                <input
+                  {...forms.update.register("name", {
+                    required: "Vui lòng nhập công việc",
+                  })}
+                  type="text"
+                  placeholder="Thêm công việc..."
+                  className={clsx(
+                    "border  p-2 rounded outline-none",
+                    forms.update.errors?.name
+                      ? "border-red-500"
+                      : "border-gray-300"
+                  )}
+                  defaultValue={t.name}
+                />
+                <input
+                  type="text"
+                  {...forms.update.register("id")}
+                  defaultValue={t.id}
+                  hidden
+                />
+                <input
+                  type="text"
+                  defaultValue={"add"}
+                  {...forms.add.register("action")}
+                  hidden
+                />
+                <div className="flex items-center *:px-5 *:py-2 *:text-white *:rounded space-x-5">
+                  <button
+                    type="button"
+                    onClick={() => setIsOpen({ open: false, id: null })}
+                    className="bg-red-500"
+                  >
+                    Huỷ
+                  </button>
+                  <button type="submit" className="bg-blue-500">
+                    Cập nhập
+                  </button>
+                </div>
+              </form>
+            )}
           </li>
         ))}
       </ul>
